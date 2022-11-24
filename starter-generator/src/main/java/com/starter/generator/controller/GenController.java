62f5790c36923a5053fc3c9df7a1b69e49d3cc1a
wangzehui <18500507445@163.com>
2022-11-24 22:33:39 +0800

    修改生成器

--- starter-generator/src/main/java/com/starter/generator/controller/GenController.java
+++ starter-generator/src/main/java/com/starter/generator/controller/GenController.java
@@ -1,25 +1,5 @@
 package com.starter.generator.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import javax.servlet.http.HttpServletResponse;
-
-import com.starter.generator.domain.GenTable;
-import com.starter.generator.domain.GenTableColumn;
-import org.apache.commons.io.IOUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.authz.annotation.RequiresRoles;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.druid.DbType;
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
@@ -35,8 +15,24 @@ import com.starter.common.enums.BusinessType;
 import com.starter.common.utils.StringUtils;
 import com.starter.common.utils.security.PermissionUtils;
 import com.starter.common.utils.sql.SqlUtil;
+import com.starter.generator.domain.GenTable;
+import com.starter.generator.domain.GenTableColumn;
 import com.starter.generator.service.IGenTableColumnService;
 import com.starter.generator.service.IGenTableService;
+import org.apache.commons.io.IOUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 代码生成 操作处理
