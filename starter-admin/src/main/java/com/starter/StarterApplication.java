49f0126b13012dc223e5ac17771034d9d27f5b22
wangzehui <18500507445@163.com>
2022-11-24 20:58:02 +0800

    调整目录

---
+++ starter-admin/src/main/java/com/starter/StarterApplication.java
@@ -0,0 +1,18 @@
+package com.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class StarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class, args);
+    }
+}
