fd8d746d4e0f9c04f5f2512c6b43325827af3370
wangzehui <18500507445@163.com>
2022-11-26 17:02:32 +0800

    修改pom

---
+++ starter-api/src/main/java/com/starter/StarterApiApplication.java
@@ -0,0 +1,18 @@
+package com.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 后台启动程序
+ *
+ * @author wzh
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class StarterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApiApplication.class, args);
+    }
+}
---
+++ starter-api/src/main/java/com/starter/web/controller/tool/SwaggerController.java
@@ -0,0 +1,22 @@
+package com.starter.web.controller.tool;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.starter.common.core.controller.BaseController;
+
+/**
+ * swagger 接口
+ *
+ * @author wzh
+ */
+@Controller
+@RequestMapping("/tool/swagger")
+public class SwaggerController extends BaseController {
+    @RequiresPermissions("tool:swagger:view")
+    @GetMapping()
+    public String index() {
+        return redirect("/swagger-ui/index.html");
+    }
+}
